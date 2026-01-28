39d50563162f4585c87bdb6bccee9e1997bd4cc8
https://github.com/imranakbara-lab/guvihtmltask.git <imranakbar.a@gmail.com>
2026-01-28 17:28:25 +0530

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+package quvitaskfor;
+
+public class Reverse {
+	
+	public static void main (String[] args) {
+		
+		int number=876;
+		int reverse=0;
+		
+		while (number !=0) {
+			
+			reverse=reverse*10+(number%10);
+			number = number/10;
+		}
+	
+	     System.out.println(reverse);
+	}
+	
+
+}
