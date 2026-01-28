d0a2c54a7c71394cb77fe932fcfbb0df7f8fd206
https://github.com/imranakbara-lab/guvihtmltask.git <imranakbar.a@gmail.com>
2026-01-28 17:21:38 +0530

    ifandwhileloop.java

---
+++ ifandwhileloop.java
@@ -0,0 +1,22 @@
+package quvitaskfor;
+
+public class ifandwhileloop {
+
+	public static void main (String[] args) {
+		
+		System.out.println("USING FOR LOOP:");
+		for (int i=10; i<=50; i++) {
+		System.out.println(i);
+		}
+		
+		System.out.println("USING WHILE LOOP");
+		int j=10;
+		while(j<=50) {
+			System.out.println(j);
+			j++;
+		}
+		
+		
+	}
+	
+}
