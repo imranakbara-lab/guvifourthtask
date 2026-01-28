ca10edbdc79e49b75c074e52a30e98e4e66eba35
https://github.com/imranakbara-lab/guvihtmltask.git <imranakbar.a@gmail.com>
2026-01-28 17:26:41 +0530

    PositiveNegavtive.java

---
+++ PositiveNegavtive.java
@@ -0,0 +1,30 @@
+package quvitaskfor;
+
+public class PositiveNegavtive {
+	
+	public static void main (String[] args) {
+		
+		int number1=123;
+		int number2=-100;
+		
+		if (number1>0) {
+			System.out.println(number1+" is POSITIVE");
+		}else if (number1<0) {
+		System.out.println(number1+" is NEGATIVE");
+		
+	}else {
+		System.out.println(number1+"is ZERO");
+	}
+	
+		
+	if (number2>0) {
+		System.out.println(number2+"is POSITIVE");
+	}else if(number2<0) {
+		System.out.println(number2+" is NEGATIVE");
+	}else {
+		System.out.println(number2+"is ZERO");
+	}
+		}
+}
+	
+
