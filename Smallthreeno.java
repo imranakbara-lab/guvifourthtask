d6de0062bb4862df36f50db4a7d51020f76cd118
https://github.com/imranakbara-lab/guvihtmltask.git <imranakbar.a@gmail.com>
2026-01-28 17:31:59 +0530

    Smallthreeno.java

---
+++ Smallthreeno.java
@@ -0,0 +1,25 @@
+package quvitaskfor;
+
+public class Smallthreeno {
+
+	
+	public static void main(String[] args) {
+		
+		int a=56;
+		int b=17;
+		int c=25;
+		
+		if(a<=b && a<=c) {
+			System.out.println("A - is Smallest Number");
+				
+		} else if (b<=a && b<=c) {
+			
+			System.out.println(" B - is Smallest Number");
+			
+		}else {
+			System.out.println("C - is Smallest Number");
+		}
+			
+		
+	}
+}
